0377342a50adf57f42d60949bae18dcd1d4dadc4
Victor Hristoskov <victor_hristoskov@mail.bg>
2013-06-28 22:15:14 +0300

    Added multithreaded dataManager. functions reading/writing to plist

---
+++ MathArt/MathArt/MADataManager.h
@@ -0,0 +1,13 @@
+//
+//  MADataManager.h
+//  MathArt
+//
+//  Created by Victor Hristoskov on 6/28/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MADataManager : NSObject
+
+@end
---
+++ MathArt/MathArt/MADataManager.m
@@ -0,0 +1,13 @@
+//
+//  MADataManager.m
+//  MathArt
+//
+//  Created by Victor Hristoskov on 6/28/13.
+//  Copyright (c) 2013 MentorMate. All rights reserved.
+//
+
+#import "MADataManager.h"
+
+@implementation MADataManager
+
+@end
